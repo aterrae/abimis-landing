995716b033d95700c70b4c381ab4c698ae533fa2
Ayoub Aabass <aabass.ayoub@gmail.com>
2017-01-17 10:55:00 +0100

    FIXED: several responsiveness bugs

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -2,10 +2,12 @@ $(document).ready(function() {
     $(".contactCTA").click(function() {
         $(".overlay").addClass("overlay--visible");
         $("body").addClass("noscroll");
+        $(".cover").addClass("cover--hidden");
     });
 
     $(".overlay__close").click(function() {
         $(".overlay").removeClass("overlay--visible");
         $("body").removeClass("noscroll");
+        $(".cover").removeClass("cover--hidden");
     })
 })

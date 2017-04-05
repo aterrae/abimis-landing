c94f3c8ca4f94c33ce543ec7d00b2fec488adfac
Ayoub Aabass <aabass.ayoub@gmail.com>
2017-04-05 10:08:42 +0200

    ADDED: countdown component

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
+import STRINGS from '../data/strings';
+
 function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     if (/windows phone/i.test(userAgent)) {
         return "Windows Phone";
@@ -44,5 +46,35 @@ $(document).ready(function() {
         $(".overlay").removeClass("overlay--visible");
         $("body").removeClass("noscroll");
         $(".cover").removeClass("cover--hidden");
-    })
+    });
+    initializeClock('countdown', STRINGS.countdown.deadline);
 });
+
+function getTimeRemaining(endtime){
+    var offset = Date.parse(endtime) - Date.parse(new Date());
+    var parsedOffset = {};
+    parsedOffset.total = offset;
+    parsedOffset.seconds = Math.floor( (offset/1000) % 60 );
+    parsedOffset.minutes = Math.floor( (offset/1000/60) % 60 );
+    parsedOffset.hours = Math.floor( (offset/(1000*60*60)) % 24 );
+    parsedOffset.days = Math.floor( offset/(1000*60*60*24) );
+    return parsedOffset;
+}
+
+function initializeClock(id, endtime){
+    var clock = document.getElementById(id);
+    var days = clock.querySelector('#countdown_days')
+    var hours = clock.querySelector('#countdown_hours')
+    var minutes = clock.querySelector('#countdown_minutes')
+    var seconds = clock.querySelector('#countdown_seconds')
+    var timeinterval = setInterval(function(){
+        var offset = getTimeRemaining(endtime);
+        days ? days.innerHTML = offset.days : null;
+        hours ? hours.innerHTML = offset.hours : null;
+        minutes ? minutes.innerHTML = offset.minutes : null;
+        seconds ? seconds.innerHTML = offset.seconds : null;
+        if(offset.total<=0){
+            clearInterval(timeinterval);
+        }
+    },1000);
+}
